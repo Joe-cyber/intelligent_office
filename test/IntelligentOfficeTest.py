98dcce820a5fc51828c5aadb3438d0988be7f162
Joe-cyber <caliendo.gioacchino00@gmail.com>
2022-12-06 17:16:11 +0100

    testing user story one

--- test/IntelligentOfficeTest.py
+++ test/IntelligentOfficeTest.py
@@ -10,4 +10,21 @@ class IntelligentOfficeTest(unittest.TestCase):
     """
     Define your test cases here
     """
-    pass
+    def setUp(self) -> None:
+        self.pg = IntelligentOffice()
+
+    @patch.object(GPIO, 'input')
+    def test_check_quadrant_occupancy_pin_one_false(self, mock_input):
+        mock_input.return_value = 0
+        occ = self.pg.check_quadrant_occupancy(self.pg.INFRARED_PIN_1)
+        self.assertFalse(occ)
+
+    @patch.object(GPIO, 'input')
+    def test_check_quadrant_occupancy_pin_one_true(self, mock_input):
+        mock_input.return_value = 50
+        occ = self.pg.check_quadrant_occupancy(self.pg.INFRARED_PIN_1)
+        self.assertTrue(occ)
+
+    def test_check_quadrant_occupancy_wrong_pin(self):
+        self.assertRaises(IntelligentOfficeError, self.pg.check_quadrant_occupancy, 10)
+
